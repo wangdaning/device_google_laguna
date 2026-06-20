91e0ff95ffd3547fce22c1e665b41271e6506550
wangdaning <mynamewasgrimm@gmail.com>
2026-06-20 18:24:59 +0800

    Add Network Fallback
    -Enable VoLTE
    -Force fallback to 5G features enabled when SIM card not recognized by google
    -Allow to dynamically add 5G to carriers in unsupported countries

--- common.mk
+++ common.mk
@@ -361,6 +361,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     AmbientCueOverlay \
+    CarrierConfigOverlay \
     DMServiceOverlayProductLaguna \
     EuiccSupportPixelOverlay \
     FrameworkResOverlayProductLaguna \
